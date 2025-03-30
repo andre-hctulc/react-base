4d09e3d008553647ce694d7a5bb0a89e72e077e0
andre-hctulc <andre.tho.44@gmail.com>
2025-03-30 01:18:28 +0100

    >>

--- src/components/data-display/data-grid/data-grid.tsx
+++ src/components/data-display/data-grid/data-grid.tsx
@@ -87,11 +87,11 @@ export const DataGrid = <T extends object>(props: DataGridProps<T>) => {
     const ranId = useId();
     const id = props.storage?.id ?? ranId;
     const [selection, setSelection] = useState<T[]>([]);
-    const selectionSet = useMemo(() => new Set(selection.map(row => getRowId.current(row))), [selection]);
+    const selectionSet = useMemo(() => new Set(selection.map((row) => getRowId.current(row))), [selection]);
     const [hiddenCols, setHiddenCols] = usePersistentState(
         withPrefix("data-grid-hide-cols_") + id,
         props.defaultHideCols || [],
-        props.storage?.engine || usePersistentState.memoryStorage
+        props.storage?.engine
     );
     const len = props.rows.length;
     const empty = len === 0;
@@ -108,12 +108,23 @@ export const DataGrid = <T extends object>(props: DataGridProps<T>) => {
             c.push({
                 hidable: false,
                 label: settingsEnabled && (
-                    <DataGridSettings cols={c} hiddenCols={hiddenCols} icon={props.components?.settingsIcon} onChange={hiddenCols => setHiddenCols(hiddenCols)} />
+                    <DataGridSettings
+                        cols={c}
+                        hiddenCols={hiddenCols}
+                        icon={props.components?.settingsIcon}
+                        onChange={(hiddenCols) => setHiddenCols(hiddenCols)}
+                    />
                 ),
                 render:
                     props.rowActionCell ||
                     ((cell, row, col) =>
-                        props.rowActionCellPopover && <DataGridActions moreIcon={props.components?.moreIcon} render={props.rowActionCellPopover} row={row} />),
+                        props.rowActionCellPopover && (
+                            <DataGridActions
+                                moreIcon={props.components?.moreIcon}
+                                render={props.rowActionCellPopover}
+                                row={row}
+                            />
+                        )),
                 path: "$actions$",
                 width: 50,
                 className: "flex justify-center items-center",
@@ -137,7 +148,7 @@ export const DataGrid = <T extends object>(props: DataGridProps<T>) => {
                                 setSelection(newSelection);
                                 props.onSelectionChange?.(newSelection);
                             }}
-                            onClick={e => e.stopPropagation()}
+                            onClick={(e) => e.stopPropagation()}
                         />
                     ),
                 path: "$select$",
@@ -151,11 +162,13 @@ export const DataGrid = <T extends object>(props: DataGridProps<T>) => {
                         value={selectionSet.has(getRowId.current(row))}
                         onChange={({ value }) => {
                             const rowId = getRowId.current(row);
-                            const newSelection = value ? [...selection, row] : selection.filter(r => getRowId.current(r) !== rowId);
+                            const newSelection = value
+                                ? [...selection, row]
+                                : selection.filter((r) => getRowId.current(r) !== rowId);
                             setSelection(newSelection);
                             props.onSelectionChange?.(newSelection);
                         }}
-                        onClick={e => e.stopPropagation()}
+                        onClick={(e) => e.stopPropagation()}
                     />
                 ),
                 stickyLeft: true,
@@ -165,14 +178,24 @@ export const DataGrid = <T extends object>(props: DataGridProps<T>) => {
         const hideSet = new Set(hiddenCols);
 
         return c
-            .filter(col => !hideSet.has(col.path))
-            .map(col => {
+            .filter((col) => !hideSet.has(col.path))
+            .map((col) => {
                 return {
                     ...props.baseColDef,
                     ...col,
                 };
             });
-    }, [props.cols, hiddenCols, props.rowActionCellPopover, props.rowActionCell, props.selectable, props.selectAll, selectionSet, props.rows, selection]);
+    }, [
+        props.cols,
+        hiddenCols,
+        props.rowActionCellPopover,
+        props.rowActionCell,
+        props.selectable,
+        props.selectAll,
+        selectionSet,
+        props.rows,
+        selection,
+    ]);
 
     const updateScrollBoundaries = () => {
         if (!scrollXBox.current) return;
@@ -206,7 +229,7 @@ export const DataGrid = <T extends object>(props: DataGridProps<T>) => {
         const newRowIds = new Set(props.rows.map(getRowId.current));
 
         // Unselect removed rows
-        const newSelection = selection.filter(row => newRowIds.has(getRowId.current(row)));
+        const newSelection = selection.filter((row) => newRowIds.has(getRowId.current(row)));
 
         // If selection changed (rows removed from selection), update
         if (newSelection.length !== selection.length) {
@@ -218,18 +241,36 @@ export const DataGrid = <T extends object>(props: DataGridProps<T>) => {
     const showPlaceholder = empty || props.loading;
 
     return (
-        <div className={clsx("bg-paper flex flex-col relative overflow-y-auto min-h-0", showPlaceholder && "min-h-40", props.className)}>
+        <div
+            className={clsx(
+                "bg-paper flex flex-col relative overflow-y-auto min-h-0",
+                showPlaceholder && "min-h-40",
+                props.className
+            )}
+        >
             {showPlaceholder && (
                 <Placeholder className="absolute w-full h-full left-0">
-                    {!props.loading ? empty && (props.components?.empty || "No data found") : props.components?.loading || <Spinner size="2xl" />}
+                    {!props.loading
+                        ? empty && (props.components?.empty || "No data found")
+                        : props.components?.loading || <Spinner size="2xl" />}
                 </Placeholder>
             )}
-            <div className={clsx("grow overflow-x-auto relative flex flex-col")} style={{}} ref={scrollXBox} onScroll={() => updateScrollBoundaries()}>
+            <div
+                className={clsx("grow overflow-x-auto relative flex flex-col")}
+                style={{}}
+                ref={scrollXBox}
+                onScroll={() => updateScrollBoundaries()}
+            >
                 <div className="shrink-0" style={{ minWidth: "min-content" }}>
-                    <DataGridHeaderRow leftEnd={leftEnd} rightEnd={rightEnd} cols={cols} height={props.headerRowHeight} />
+                    <DataGridHeaderRow
+                        leftEnd={leftEnd}
+                        rightEnd={rightEnd}
+                        cols={cols}
+                        height={props.headerRowHeight}
+                    />
                     {!props.loading &&
                         len > 0 &&
-                        props.rows.map(row => (
+                        props.rows.map((row) => (
                             <DataGridRow
                                 leftEnd={leftEnd}
                                 rightEnd={rightEnd}
