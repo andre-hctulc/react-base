bf246d63804acb210351156a451d10d397dacedc
andre-hctulc <andre.tho.44@gmail.com>
2025-04-14 02:47:16 +0200

    >>

--- src/components/containers/index.ts
+++ src/components/containers/index.ts
@@ -3,6 +3,7 @@ export * from "./center.js";
 export * from "./card.js";
 export * from "./card-header.js";
 export * from "./card-footer.js";
+export * from "./card-body.js";
 export * from "./card-header.js";
 export * from "./list.js";
 export * from "./flex.js";
