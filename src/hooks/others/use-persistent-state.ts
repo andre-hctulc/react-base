dac5ea188dbf2b9320445c49ccbf05e870bb282a
andre-hctulc <andre.tho.44@gmail.com>
2025-04-21 01:16:26 +0200

    >>

--- src/hooks/others/use-persistent-state.ts
+++ src/hooks/others/use-persistent-state.ts
@@ -1,12 +1,14 @@
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState, type Dispatch } from "react";
 
-type SetPersistentStateDispatch<T> = (value: (T | undefined) | ((previousValue: T) => T)) => void;
-
+/**
+ * Persists the current state. 
+ * If the state is undefined, the item is removed from the storage.
+ */
 export function usePersistentState<T>(
     key: string,
     defaultValue: T,
     storage?: Storage
-): [T, SetPersistentStateDispatch<T>] {
+): [T, Dispatch<React.SetStateAction<T>>] {
     const [state, setState] = useState<T>(() => {
         // Get initial state from storage or use the default value
         if (!storage) storage = localStorage;
@@ -17,29 +19,11 @@ export function usePersistentState<T>(
         return defaultValue;
     });
 
-    const setValue = useCallback(
-        (value: Parameters<SetPersistentStateDispatch<T>>[0]) => {
-            // If the value is a function, pass it as is. It must always return a value.
-            if (typeof value === "function") {
-                setState(value);
-                return;
-            }
-
-            // When explicitly setting undefined, remove the item from storage
-            if (value === undefined) {
-                if (!storage) storage = localStorage;
-                storage.removeItem(key);
-                setState(defaultValue);
-            } else {
-                setState(value);
-            }
-        },
-        [defaultValue, storage, key]
-    );
-
     useEffect(() => {
         if (!storage) storage = localStorage;
-        if (state !== undefined) {
+        if (state === undefined) {
+            storage.removeItem(key);
+        } else {
             storage.setItem(key, JSON.stringify(state));
         }
     }, [key, state, storage]);
@@ -47,7 +31,7 @@ export function usePersistentState<T>(
     useEffect(() => {
         const handleStorage = (event: StorageEvent) => {
             if (event.storageArea === storage && event.key === key) {
-                setState(event.newValue ? JSON.parse(event.newValue) : undefined);
+                setState(event.newValue ? JSON.parse(event.newValue) : defaultValue);
             }
         };
 
@@ -58,5 +42,5 @@ export function usePersistentState<T>(
         };
     }, [key, storage]);
 
-    return [state, setValue];
+    return [state, setState];
 }
