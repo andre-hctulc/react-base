a97e216b4eb12d645196ffaceb362758314ea8c6
andre-hctulc <andre.tho.44@gmail.com>
2025-06-28 00:27:24 +0200

    >>

--- src/components/exotic/csrf-input.tsx
+++ src/components/exotic/csrf-input.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import { HiddenInput } from "./hidden-input.js";
+import { HiddenInput } from "../input/hidden-input.js";
 
 interface CSRFInputProps {
     /**
