faecc14db8b02c6804bc969988af7b72a827132c
andre-hctulc <andre.tho.44@gmail.com>
2026-08-05 16:13:54 +0200

    chore: update files

--- src/components/table-placeholder.tsx
+++ src/components/table-placeholder.tsx
@@ -1,16 +1,16 @@
-import type { ComponentProps } from "react";
+import type { ComponentProps, FC } from "react";
 import { TableCell, TableRow } from "@/components/ui/table.js";
 
 interface TablePlaceholderProps extends ComponentProps<typeof TableRow> {
     colSpan?: number;
 }
 
-export function TablePlaceholder({ colSpan, ...props }: TablePlaceholderProps) {
+export const TablePlaceholder: FC<TablePlaceholderProps> = ({ colSpan, children, ...props }) => {
     return (
         <TableRow {...props}>
             <TableCell colSpan={colSpan ?? 1000} className="h-24 text-center italic text-muted-foreground">
-                No data found
+                {children || "No data found"}
             </TableCell>
         </TableRow>
     );
-}
+};
