a07eb209030dddf8223541df591d164767a7caf1
andre-hctulc <andre.tho.44@gmail.com>
2026-08-10 01:28:28 +0200

    chore: update files

--- src/components/select-factory.tsx
+++ src/components/select-factory.tsx
@@ -22,11 +22,11 @@ export type SelectItemGroupMap = { [groupLabel: string]: SelectItem[] };
 export type SelectItems = SelectItemList | SelectItemGroupMap;
 
 interface SelectFactoryProps extends ComponentProps<typeof Select> {
-    options: SelectItems;
+    items: SelectItems;
 }
 
-export const SelectFactory: FC<SelectFactoryProps> = ({ options, ...props }) => {
-    const optionsLength = Array.isArray(options) ? options.length : Object.keys(options).length;
+export const SelectFactory: FC<SelectFactoryProps> = ({ items, ...props }) => {
+    const len = Array.isArray(items) ? items.length : Object.keys(items).length;
 
     return (
         <Select {...props}>
@@ -34,17 +34,17 @@ export const SelectFactory: FC<SelectFactoryProps> = ({ options, ...props }) => 
                 <SelectValue />
             </SelectTrigger>
             <SelectContent>
-                {Array.isArray(options) ? (
+                {Array.isArray(items) ? (
                     <SelectGroup>
-                        {options.map((item) => (
+                        {items.map((item) => (
                             <SelectItem key={item.value} value={item.value}>
                                 {item.label}
                             </SelectItem>
                         ))}
                     </SelectGroup>
                 ) : (
-                    Object.entries(options).map(([groupLabel, groupOptions]) => {
-                        const isLast = Object.keys(options).indexOf(groupLabel) === optionsLength - 1;
+                    Object.entries(items).map(([groupLabel, groupOptions]) => {
+                        const isLast = Object.keys(items).indexOf(groupLabel) === len - 1;
                         return (
                             <Fragment key={groupLabel}>
                                 {!isLast && <SelectSeparator />}
